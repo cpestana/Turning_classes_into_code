b25c9040001e6677ac34e8eee30b2ffee41dfb19
Christine Pestana <clpestanaa@gmail.com>
2021-05-31 11:37:22 -0700

    Code for classes commit

---
+++ order.py
@@ -0,0 +1,25 @@
+
+class user():
+    def __init__(self, orderid, totalamount)
+        self.orderid=orderid
+        self.orderdate=datetime.date.today()
+        self.orderstatus=''
+        self.totalamount=totalamount
+
+    def getOrderID(self):
+        return self.orderid
+
+    def getOrderDate(self):
+        return self.orderdate
+
+    def getOrderStatus(sefl):
+        return self.orderstatus
+
+    def setOrderStatus(self):
+        self.orderstatus=orderstatus
+
+    def getTotalAmount(self):
+        return self.totalamount
+
+    
+
---
+++ user.py
@@ -0,0 +1,45 @@
+
+user.py
+
+class user():
+    def __init__(self, userid)
+        self.userid=userid
+        self.userpassword='P@ssword1'
+        self.username='cpestana'
+        self.lastname='Pestana'
+        self.firstname='Christine'
+        self.email='cpestana@yahoo.com'
+
+    def getuserID(self):
+        return self.userid
+
+    def getUserPassword(self):
+        return self.userpassword
+
+    def setUserPassword(self, userpassword):
+        self.userpassword=userpassword
+
+    def getUserName(sefl):
+        return self.username
+
+    def setUserName(self):
+        self.username=username
+
+    def getLastName(self):
+        return self.lastname
+
+    def setLastName(self):
+        self.lastname=lastname
+
+    def getFirstName(self):
+        return self.firstname
+    
+    def setFirstName(self):
+        self.firstname=firstname
+
+    def getEmail(self):
+        return self.email
+
+    def setEmail(self):
+        self.email=email
+
